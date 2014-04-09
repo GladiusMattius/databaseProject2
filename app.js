eb4f5d943f83d7bc7c52c1ec7619dff1ef92a571
GladiusMattius <teammmapps@gmail.com>
2014-04-09 15:20:50 -0700

    added store functionality

--- app.js
+++ app.js
@@ -45,6 +45,10 @@ app.get('/createAccount', function(req, res) {
     res.sendfile(__dirname + '/createAccount.html');
 });
 
+app.get('/store', function(req, res) {
+    res.sendfile(__dirname + '/store.html');
+});
+
 // Update MySQL database
 
 /*app.post('/', function (req, res) {
@@ -77,7 +81,59 @@ app.post('/createAccount', function (req, res) {
         }
     );
 });
+app.post('/store', function (req, res) {
+    console.log(req.body);
+          connection.query('select * from Item where ItemID = ?', req.body.itemtype, 
+			     function (err, result) {
+                    console.log(result);
+                    if(result.length > 0) {
+			              res.send('Name: ' + result[0].Name + '<br />' +
+					              'Description: ' + result[0].Description + '<br />' 
+					             );
+                    }
+                    else
+                      res.send('No items found.');
+				 });    
+});
+/*
+app.post('/store', function (req, res) {
+    console.log(req.body);
+    
+    // get user by id
+    if(typeof req.body.id != 'undefined') {
+        connection.query('select * from users where id = ?', req.body.id, 
+            function (err, result) {
+                console.log(result);
+                if(result.length > 0) {
+                    var responseHTML = '<table class="users"><tr><th>ID</th><th>Username</th><th>Password</th></tr>';
+                    responseHTML += '<tr><td>' + result[0].id + '</td>' + 
+                                    '<td>' + result[0].username + '</td>' +
+                                    '<td>' + result[0].password + '</td>' +
+                                    '</tr></table>';
+                    res.send(responseHTML);
+                }
+                else
+                  res.send('User does not exist.');
+            }
+        );     
+    }
+    //get user by username    
+    else if( typeof req.body.username != 'undefined') {
+        connection.query('select username, password from users where username = ?', req.body.username, 
+            function (err, result) {
+                console.log(result);
+                if(result.length > 0) {
+  	              res.send('Username: ' + result[0].username + '<br />' +
+		  	       'Password: ' + result[0].password
+                );
+            }
+            else
+                res.send('User does not exist.');
+		});
+    }
+});
 
+*/
 // Begin listening
 
 app.listen(8009);
